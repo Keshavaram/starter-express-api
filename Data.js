c5568e23639e3b94641e5fb113ee91308e8bd898
Keshavaram <keshavjayanth1401@gmail.com>
2022-10-24 22:40:49 +0530

    first

---
+++ Data.js
@@ -0,0 +1,175 @@
+export const Restaurants_data = [
+    {
+        name: "Meghana Foods",
+        rating: 4.6,
+        cuisine: "South Indian, Sea Food, North Indian",
+        eta: 50,
+        price: 500,
+        image: "meghana_foods.jpg"
+    },
+    {
+        name: "Chai Point",
+        rating: 3.1,
+        cuisine: "South Indian, Sea Food, North Indian",
+        eta: 20,
+        price: 100,
+        image: "tea.jpg"
+    },
+    {
+        name: "Asha Tiffins",
+        rating: 2.8,
+        cuisine: "South Indian, Sea Food, North Indian",
+        eta: 50,
+        price: 200,
+        image: "tiffin.jpg"
+    },
+    {
+        name: "Venezia",
+        rating: 4.7,
+        cuisine: "American",
+        eta: 60,
+        price: 1500,
+        image: "clam.JPG"
+    },
+    {
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },{
+        name: "Vito Marcello's Italian Bistro",
+        rating: 4.6,
+        cuisine: "Italian",
+        eta: 30,
+        price: 2000,
+        image: "italian.jpg"
+    },
+]
--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express')
+import cors from "cors";
+import express from "express";
+import fs from "fs";
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+import {Restaurants_data} from "./Data.js"
+
+app.use(cors())
+app.use("/carouselImages",express.static("CarouselImages"))
+app.use('/restaurantImages', express.static('RestaurantImages'));
+app.use('/icons',express.static('icons'))
+
+
+app.get("/carouselFiles",async (req,res) =>
+{
+    const testFolder = './CarouselImages';
+    let imageNames = []
+    fs.readdirSync(testFolder).forEach(file => {
+        imageNames.push(file)
+    });
+
+    res.send({images: imageNames.toString()})
 })
-app.listen(process.env.PORT || 3000)
+
+app.get("/restaurantData", async (req, res) => {
+    res.send(Restaurants_data)
+})
+
+app.listen(8080,() =>
+{
+    console.log("Server running at 8080");
+});
